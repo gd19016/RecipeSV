8ba21592aa698eb566a4292497dbb4067c553128
HM11019 <hm11019@ues.edu.sv>
2022-06-12 01:42:27 -0600

    Adicion de Libreria iText en Fragment Usuarios: - Creacion de logica para reporteria PDF's para la creacion de archivo externo que contendra los usuarios creados.

--- app/src/main/java/sv/edu/ues/fia/eisi/recipesv/ui/usuario/UsuarioFragment.kt
+++ app/src/main/java/sv/edu/ues/fia/eisi/recipesv/ui/usuario/UsuarioFragment.kt
@@ -1,22 +1,38 @@
 package sv.edu.ues.fia.eisi.recipesv.ui.usuario
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.itextpdf.text.*
+import com.itextpdf.text.pdf.PdfPTable
+import com.itextpdf.text.pdf.PdfWriter
 import sv.edu.ues.fia.eisi.recipesv.R
 import sv.edu.ues.fia.eisi.recipesv.RegistroRecetaApplication
 import sv.edu.ues.fia.eisi.recipesv.db.UsuarioEntity
+import java.io.File
+import java.io.FileOutputStream
 
 class UsuarioFragment : Fragment(), UsuarioListAdapter.OnUsuarioClickListener {
+
+    var listaUsuarios: ArrayList<UsuarioEntity> = ArrayList()
 
     companion object {
         fun newInstance() = UsuarioFragment()
@@ -42,16 +58,33 @@ class UsuarioFragment : Fragment(), UsuarioListAdapter.OnUsuarioClickListener {
         recyclerView.layoutManager = LinearLayoutManager(context)
         viewModel.usuarios.observe(viewLifecycleOwner, Observer { usuarios -> usuarios?.let { adapter.submitList(it) }
         })
+
         val fab = view.findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
             viewModel.usuarioActual = null
             findNavController().navigate(R.id.action_nav_usuario_to_nav_guardar_usuario)
         }
+
+        //iTextPDF
+        if(checkPermission()) {
+            Toast.makeText(context, "Permiso Aceptado", Toast.LENGTH_LONG).show();
+        } else {
+            requestPermissions();
+        }
+
+        val btnCrearPdf: Button = view.findViewById(R.id.btnCrearPdf)
+        btnCrearPdf.setOnClickListener {
+            viewModel.usuarioActual = null
+            crearPdf()
+        }
+
     }
+
     override fun onEditUsuarioClicked(usuario: UsuarioEntity) {
         viewModel.usuarioActual = usuario
         findNavController().navigate(R.id.action_nav_usuario_to_nav_guardar_usuario)
     }
+
     override fun onDeleteUsuarioClicked(usuario: UsuarioEntity) {
         val builder = AlertDialog.Builder(activity)
         builder.setMessage("Estas seguro que deseas borrar el usuario con correo: ${usuario.email}?")
@@ -62,5 +95,87 @@ class UsuarioFragment : Fragment(), UsuarioListAdapter.OnUsuarioClickListener {
             }
         val alert = builder.create()
         alert.show()
+    }
+
+    //iTextPDF
+    fun crearPdf() {
+        var path = Environment.getExternalStorageDirectory().absolutePath+"/EjemploITextPDF"
+
+        val dir = File(path)
+        if (!dir.exists())
+            dir.mkdirs()
+
+        val file = File(dir, "usuarios.pdf")
+        val fileOutputStream = FileOutputStream(file)
+
+        val documento = Document()
+        PdfWriter.getInstance(documento, fileOutputStream)
+
+        documento.open()
+
+        val titulo = Paragraph(
+            "Lista de Usuarios \n\n\n",
+            FontFactory.getFont("arial", 22f, Font.BOLD, BaseColor.BLUE)
+        )
+
+        documento.add(titulo)
+
+        var tabla = PdfPTable(3)
+        tabla.addCell("USUARIO")
+        tabla.addCell("EMAIL")
+        tabla.addCell("CONTRASEÑA")
+
+        for (item in listaUsuarios){
+            tabla.addCell(item.nombre)
+            tabla.addCell(item.email)
+            tabla.addCell(item.password)
+        }
+
+        documento.add(tabla)
+
+        documento.close()
+    }
+
+    private fun checkPermission(): Boolean {
+        val permission1 =
+            context?.let {
+                ContextCompat.checkSelfPermission(it,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        val permission2 =
+            context?.let {
+                ContextCompat.checkSelfPermission(it,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE),
+            200
+        )
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 200) {
+            if (grantResults.size > 0) {
+                val writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                val readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED
+                if (writeStorage && readStorage) {
+                    Toast.makeText(context, "Permiso concedido", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context, "Permiso denegado", Toast.LENGTH_LONG).show()
+                    //finish()
+                }
+            }
+        }
     }
 }
